143e69c76017d612047fbfd53696fd3bf0272e0c
yummy2eat <saishravanm@gmail.com>
2021-01-06 14:39:30 -0600

    Add files via upload

---
+++ i3setupthing.sh
@@ -0,0 +1,7 @@
+sudo pacman -Sy xorg xorg-xinit picom
+sudo pacman -Sy firefox
+sudo pacman -Sy gimp
+sudo pacman -Sy vim
+sudo pacman -Sy i3
+sudo pacman -Sy dmenu nitrogen termite
+sudo pacman -Sy lightdm-gtk-greeter lightdm-gtk-greeter-settings
